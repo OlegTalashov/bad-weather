d1960559203629bd6feb0ba61a167e2e21519e7c
Олег Талашов <oleg.igorevich.talashov@gmail.com>
2023-02-04 20:12:05 +0400

    Testing output

---
+++ src/BadWeather.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="remove-this-div">
+      <Header />
+      {{ TestMsg }}
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Header from './assets/header/Header.vue';
+
+@Component({
+  name: 'Bad Weather',
+  components: {
+    Header,
+  }
+})
+export default class BadWeather extends Vue {
+  TestMsg: string = 'TestMsgContent';
+}
+</script>
+
+<style lang="scss">
+
+/*************** TODO Remove */
+.remove-this-div{
+  position: fixed;
+  width: 320px;
+  height: 500px;
+  top: 30%;
+  left: 40%;
+  background-color: bisque;
+}
+
+</style>
---
+++ src/assets/header/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ HeaderMsg }}
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+
+    @Component
+    export default class Header extends Vue {
+        HeaderMsg: string = "HeaderMsgContent"
+    }
+
+</script>
